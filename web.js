c5f9cfa6e7017fbfeefcce38c31498965408ce69
Sharlene <17sharl@gmail.com>
2014-07-24 15:25:20 +0800

    Modify webjs

--- web.js
+++ web.js
@@ -1,15 +1,3 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
 
-app.use(logfmt.requestLogger());
-
-app.get('*', function(req, res) {
-  res.sendfile('./app/index.html'); // load our app/index.html file
-});
-
-var port = Number(process.env.PORT || 8000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var DEFAULT_PORT = process.env.PORT || 8000;
